87ca043c4990b2e0e718ddb253964322eefd2b85
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-01 14:14:17 +0000

    Filters out non-digit chars

--- src/main/scala/aoc2023/Day1SumOfCalibrationValues.scala
+++ src/main/scala/aoc2023/Day1SumOfCalibrationValues.scala
@@ -2,6 +2,6 @@ package aoc2023
 
 class Day1SumOfCalibrationValues {
 
-  def sumOfCalibrationValue(str: String): Int = str.toList.mkString("").toInt
+  def sumOfCalibrationValue(str: String): Int = str.toList.filter(_.isDigit).mkString("").toInt
 
 }
