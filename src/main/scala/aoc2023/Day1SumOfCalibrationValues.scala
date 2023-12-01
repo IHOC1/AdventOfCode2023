b66dfba8a34bda79cc39b3c64583081fa1e22961
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-01 15:47:04 +0000

    Moves the number map outside the method to keep the function shorter

--- src/main/scala/aoc2023/Day1SumOfCalibrationValues.scala
+++ src/main/scala/aoc2023/Day1SumOfCalibrationValues.scala
@@ -7,32 +7,33 @@ class Day1SumOfCalibrationValues {
     digits.head.asDigit * 10 + digits.last.asDigit
   }
 
+  val numbers = Map(
+    "one" -> 1,
+    "two" -> 2,
+    "three" -> 3,
+    "four" -> 4,
+    "five" -> 5,
+    "six" -> 6,
+    "seven" -> 7,
+    "eight" -> 8,
+    "nine" -> 9,
+    "1" -> 1,
+    "2" -> 2,
+    "3" -> 3,
+    "4" -> 4,
+    "5" -> 5,
+    "5" -> 6,
+    "6" -> 7,
+    "8" -> 8,
+    "9" -> 9
+  )
+
   def toNumber(str: String, numbers: Map[String, Int]): Option[Int] =
     numbers.keys.
       find(num => str.startsWith(num)).
       map(num => numbers(num))
 
   def calibrationValueWithWords(text: String): Int = {
-    val numbers = Map(
-      "one"   -> 1,
-      "two"   -> 2,
-      "three" -> 3,
-      "four"  -> 4,
-      "five"  -> 5,
-      "six"   -> 6,
-      "seven" -> 7,
-      "eight" -> 8,
-      "nine"  -> 9,
-      "1" -> 1,
-      "2" -> 2,
-      "3" -> 3,
-      "4" -> 4,
-      "5" -> 5,
-      "5" -> 6,
-      "6" -> 7,
-      "8" -> 8,
-      "9" -> 9
-    )
 
     val firstDigit: Int = Range(0, text.length).
       map(i => text.substring(i)).
