8dba05214149a2249eb3e1cd4a3a5694f20ab273
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-03 09:54:59 +0000

    Adds test for another 3 digit number at the beginning of the line to drive the solution

--- src/test/scala/aoc2023/day03/EnginePartNumbersTest.scala
+++ src/test/scala/aoc2023/day03/EnginePartNumbersTest.scala
@@ -6,6 +6,7 @@ class EnginePartNumbersTest() extends AnyFlatSpec {
 
   "A number on a line" should "be parsed and located" in {
     assert(EnginePartNumbers.parseLine("467") === Row(numbers = Seq(PartNumber(from = 0, to = 2, number = 467))))
+    assert(EnginePartNumbers.parseLine("512") === Row(numbers = Seq(PartNumber(from = 0, to = 2, number = 512))))
   }
 
 }
