58871e4f871bcb7e5a1a9e65e6829db40a73ee2b
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-03 10:16:22 +0000

    Refactors to parse the numbers by regex

--- src/main/scala/aoc2023/day03/EnginePartNumbers.scala
+++ src/main/scala/aoc2023/day03/EnginePartNumbers.scala
@@ -1,13 +1,23 @@
 package aoc2023.day03
 
+import scala.util.matching.Regex
+
 class EnginePartNumbers {
 
 }
 
 object EnginePartNumbers {
 
-  def parseLine(line: String): Row = Row(Seq(PartNumber(0, 2, line.toInt)))
+  val numberPattern = "[0-9]+".r
 
+  def parseLine(line: String): Row = {
+    Row(partNumbers(line))
+  }
+
+  private def partNumbers(line: String) = {
+    numberPattern.findAllIn(line).matchData.
+      map((m: Regex.Match) => PartNumber(m.start, m.end - 1, m.group(0).toInt)).toSeq
+  }
 }
 
 case class Row(numbers: Seq[PartNumber])
