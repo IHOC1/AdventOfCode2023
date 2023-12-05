fdabeaa2c343b269f2b40b4b81dc2a277d565c73
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-05 09:19:46 +0000

    Adds passing test for a second mapped value

--- src/test/scala/aoc2023/day05/SeedsTest.scala
+++ src/test/scala/aoc2023/day05/SeedsTest.scala
@@ -6,6 +6,7 @@ class SeedsTest extends AnyFlatSpec {
 
   "A mapping" should "transform all values in range" in {
     assert(Mapping(50, 98, 2).correspondingNumber(98) === 50)
+    assert(Mapping(50, 98, 2).correspondingNumber(99) === 51)
   }
 
 }
