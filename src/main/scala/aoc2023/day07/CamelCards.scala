ecc11f07c9ee56684451b1ee69c48b3ed22138d5
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-07 17:10:50 +0000

    Refactors to implement all hand types in terms of cardGroupSizes

--- src/main/scala/aoc2023/day07/CamelCards.scala
+++ src/main/scala/aoc2023/day07/CamelCards.scala
@@ -16,11 +16,12 @@ case class Hand(cards: String) {
   def handType() = {
     val groupedCards = cards.groupBy(c => c)
     val cardGroups = groupedCards.values
+    val cardGroupSizes = cardGroups.map(_.length).toSet
 
-    if (groupedCards.size == 1) FiveOfAKind
-    else if (groupedCards.size == 2 && Set(1, 4).contains(groupedCards.head._2.length)) FourOfAKind
-    else if (groupedCards.size == 2 && Set(2, 3).contains(groupedCards.head._2.length)) FullHouse
-    else if (groupedCards.size == 3 && cardGroups.map(_.length).toSet == Set(3, 1, 1)) ThreeOfAKind
+    if (cardGroupSizes == Set(5)) FiveOfAKind
+    else if (cardGroupSizes == Set(1, 4)) FourOfAKind
+    else if (cardGroupSizes == Set(2, 3)) FullHouse
+    else if (cardGroupSizes == Set(3, 1, 1)) ThreeOfAKind
   }
 
 }
