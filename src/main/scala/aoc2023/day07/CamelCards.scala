dfb8c816fb2831479c530b5177d8084689d15e09
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-07 16:50:02 +0000

    Identifies a hand as a full house

--- src/main/scala/aoc2023/day07/CamelCards.scala
+++ src/main/scala/aoc2023/day07/CamelCards.scala
@@ -1,6 +1,6 @@
 package aoc2023.day07
 
-import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind}
+import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse}
 
 class CamelCards {
 
@@ -8,7 +8,7 @@ class CamelCards {
 
 object HandType extends Enumeration {
   type HandType = Value
-  val FiveOfAKind, FourOfAKind = Value
+  val FiveOfAKind, FourOfAKind, FullHouse = Value
 }
 
 case class Hand(cards: String) {
@@ -18,6 +18,7 @@ case class Hand(cards: String) {
 
     if (groupedCards.size == 1) FiveOfAKind
     else if (groupedCards.size == 2 && Set(1, 4).contains(groupedCards.head._2.length)) FourOfAKind
+    else if (groupedCards.size == 2 && Set(2, 3).contains(groupedCards.head._2.length)) FullHouse
   }
 
 }
--- src/test/scala/aoc2023/day07/CamelCardsTest.scala
+++ src/test/scala/aoc2023/day07/CamelCardsTest.scala
@@ -1,6 +1,6 @@
 package aoc2023.day07
 
-import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind}
+import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class CamelCardsTest extends AnyFlatSpec {
@@ -8,5 +8,6 @@ class CamelCardsTest extends AnyFlatSpec {
   "A Hand" should "have a type" in {
     assert(Hand("AAAAA").handType() === FiveOfAKind)
     assert(Hand("AA8AA").handType() === FourOfAKind)
+    assert(Hand("23332").handType() === FullHouse)
   }
 }
