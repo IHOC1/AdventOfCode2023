5154efd8493f1ec3c6d3268a51cf73c181d11693
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-07 17:07:26 +0000

    Identifies a hand as three of a kind

--- src/main/scala/aoc2023/day07/CamelCards.scala
+++ src/main/scala/aoc2023/day07/CamelCards.scala
@@ -1,6 +1,6 @@
 package aoc2023.day07
 
-import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse}
+import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind}
 
 class CamelCards {
 
@@ -8,17 +8,19 @@ class CamelCards {
 
 object HandType extends Enumeration {
   type HandType = Value
-  val FiveOfAKind, FourOfAKind, FullHouse = Value
+  val FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind = Value
 }
 
 case class Hand(cards: String) {
 
   def handType() = {
     val groupedCards = cards.groupBy(c => c)
+    val cardGroups = groupedCards.values
 
     if (groupedCards.size == 1) FiveOfAKind
     else if (groupedCards.size == 2 && Set(1, 4).contains(groupedCards.head._2.length)) FourOfAKind
     else if (groupedCards.size == 2 && Set(2, 3).contains(groupedCards.head._2.length)) FullHouse
+    else if (groupedCards.size == 3 && cardGroups.map(_.length).toSet == Set(3, 1, 1)) ThreeOfAKind
   }
 
 }
--- src/test/scala/aoc2023/day07/CamelCardsTest.scala
+++ src/test/scala/aoc2023/day07/CamelCardsTest.scala
@@ -1,6 +1,6 @@
 package aoc2023.day07
 
-import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse}
+import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class CamelCardsTest extends AnyFlatSpec {
@@ -9,5 +9,6 @@ class CamelCardsTest extends AnyFlatSpec {
     assert(Hand("AAAAA").handType() === FiveOfAKind)
     assert(Hand("AA8AA").handType() === FourOfAKind)
     assert(Hand("23332").handType() === FullHouse)
+    assert(Hand("TTT98").handType() === ThreeOfAKind)
   }
 }
