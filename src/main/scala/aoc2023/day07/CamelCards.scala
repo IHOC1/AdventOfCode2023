3387dd6ddee9ed80cf1c89749502f4d4b32e2261
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-07 16:45:57 +0000

    Identifies a hand as four of a kind

--- src/main/scala/aoc2023/day07/CamelCards.scala
+++ src/main/scala/aoc2023/day07/CamelCards.scala
@@ -1,6 +1,6 @@
 package aoc2023.day07
 
-import aoc2023.day07.HandType.FiveOfAKind
+import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind}
 
 class CamelCards {
 
@@ -8,14 +8,16 @@ class CamelCards {
 
 object HandType extends Enumeration {
   type HandType = Value
-  val FiveOfAKind = Value
+  val FiveOfAKind, FourOfAKind = Value
 }
 
 case class Hand(cards: String) {
 
   def handType() = {
-    if (cards.tail.forall(card => card == cards.head))
-      FiveOfAKind
+    val groupedCards = cards.groupBy(c => c)
+
+    if (groupedCards.size == 1) FiveOfAKind
+    else if (groupedCards.size == 2 && Set(1, 4).contains(groupedCards.head._2.length)) FourOfAKind
   }
 
 }
--- src/test/scala/aoc2023/day07/CamelCardsTest.scala
+++ src/test/scala/aoc2023/day07/CamelCardsTest.scala
@@ -1,11 +1,12 @@
 package aoc2023.day07
 
-import aoc2023.day07.HandType.FiveOfAKind
+import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind}
 import org.scalatest.flatspec.AnyFlatSpec
 
 class CamelCardsTest extends AnyFlatSpec {
 
   "A Hand" should "have a type" in {
     assert(Hand("AAAAA").handType() === FiveOfAKind)
+    assert(Hand("AA8AA").handType() === FourOfAKind)
   }
 }
