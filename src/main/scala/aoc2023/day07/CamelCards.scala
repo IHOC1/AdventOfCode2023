a20817c9f22ba1c04d2552a7518be819a3bb3d24
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-07 17:19:01 +0000

    Identifies a hand as two pair

--- src/main/scala/aoc2023/day07/CamelCards.scala
+++ src/main/scala/aoc2023/day07/CamelCards.scala
@@ -1,6 +1,6 @@
 package aoc2023.day07
 
-import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind}
+import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind, TwoPair}
 
 class CamelCards {
 
@@ -8,7 +8,7 @@ class CamelCards {
 
 object HandType extends Enumeration {
   type HandType = Value
-  val FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind = Value
+  val FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind, TwoPair = Value
 }
 
 case class Hand(cards: String) {
@@ -16,12 +16,15 @@ case class Hand(cards: String) {
   def handType() = {
     val groupedCards = cards.groupBy(c => c)
     val cardGroups = groupedCards.values
-    val cardGroupSizes = cardGroups.map(_.length).toSet
+    val cardGroupSizes = cardGroups.map(_.length).toList.sorted
 
-    if (cardGroupSizes == Set(5)) FiveOfAKind
-    else if (cardGroupSizes == Set(1, 4)) FourOfAKind
-    else if (cardGroupSizes == Set(2, 3)) FullHouse
-    else if (cardGroupSizes == Set(3, 1, 1)) ThreeOfAKind
+    println(cardGroupSizes)
+
+         if (cardGroupSizes == List(5)) FiveOfAKind
+    else if (cardGroupSizes == List(1, 4)) FourOfAKind
+    else if (cardGroupSizes == List(2, 3)) FullHouse
+    else if (cardGroupSizes == List(1, 1, 3)) ThreeOfAKind
+    else if (cardGroupSizes == List(1, 2, 2)) TwoPair
   }
 
 }
--- src/test/scala/aoc2023/day07/CamelCardsTest.scala
+++ src/test/scala/aoc2023/day07/CamelCardsTest.scala
@@ -1,6 +1,6 @@
 package aoc2023.day07
 
-import aoc2023.day07.HandType.{FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind}
+import aoc2023.day07.HandType._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class CamelCardsTest extends AnyFlatSpec {
@@ -10,5 +10,6 @@ class CamelCardsTest extends AnyFlatSpec {
     assert(Hand("AA8AA").handType() === FourOfAKind)
     assert(Hand("23332").handType() === FullHouse)
     assert(Hand("TTT98").handType() === ThreeOfAKind)
+    assert(Hand("23432").handType() === TwoPair)
   }
 }
