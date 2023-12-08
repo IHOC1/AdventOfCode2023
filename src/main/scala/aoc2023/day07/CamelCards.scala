c512f776ea27d5c98ad9038a8d1e7d632f805c73
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-08 13:37:32 +0000

    Correctly calculates the total winnings for the test data

--- src/main/scala/aoc2023/day07/CamelCards.scala
+++ src/main/scala/aoc2023/day07/CamelCards.scala
@@ -9,7 +9,27 @@ class CamelCards {
 object CamelCards {
 
   def totalWinnings(fileName: String): Int = {
-    0
+    import scala.io.Source
+
+    val source = Source.fromFile("src/test/resources/Day07CamelCardsTestData.txt")
+    val handsToBids = source.getLines().map(line => parseLine(line)).toMap
+    source.close()
+
+    val hands: Iterable[Hand] = handsToBids.keys.toList.sorted
+    val ranks = 1 until handsToBids.size + 1
+
+    hands.zip(ranks).toList.
+      map(p => handsToBids(p._1) * p._2).
+      sum
+  }
+
+  def parseLine(line: String): (Hand, Int) = {
+    val parts = line.split(" ")
+
+    val hand = Hand(parts(0))
+    val bid = parts(1).toInt
+
+    hand -> bid
   }
 }
 
@@ -18,7 +38,7 @@ object HandType extends Enumeration {
   val FiveOfAKind, FourOfAKind, FullHouse, ThreeOfAKind, TwoPair, OnePair, HighCard = Value
 }
 
-case class Hand(cards: String) {
+case class Hand(cards: String) extends Comparable[Hand] {
 
   def handType() = {
     val cardGroups = cards.groupBy(c => c).values
@@ -31,6 +51,37 @@ case class Hand(cards: String) {
     else if (cardGroupSizes == List(1, 2, 2)   ) TwoPair
     else if (cardGroupSizes == List(1, 1, 1, 2)) OnePair
     else                                         HighCard
+  }
+
+  val cardToStrength = Map('A' -> 13,
+                           'K' -> 12,
+                           'Q' -> 11,
+                           'J' -> 10,
+                           'T' -> 9,
+                           '9' -> 8,
+                           '8' -> 7,
+                           '7' -> 6,
+                           '6' -> 5,
+                           '5' -> 4,
+                           '4' -> 3,
+                           '3' -> 2,
+                           '2' -> 1)
+
+  override def compareTo(other: Hand): Int = {
+    -handType().compare(other.handType()) match {
+      case       0 => compareHandsOnCardStrength(other)
+      case unequal => unequal
+    }
+  }
+
+  private def compareHandsOnCardStrength(o: Hand) = {
+    cards.zip(o.cards).
+      map(pair => compareCards(pair)).
+      find(comparison => comparison != 0).getOrElse(0)
+  }
+
+  def compareCards(cards: (Char, Char)): Int = {
+    cardToStrength(cards._1).compare(cardToStrength(cards._2))
   }
 
 }
--- src/test/scala/aoc2023/day07/CamelCardsTest.scala
+++ src/test/scala/aoc2023/day07/CamelCardsTest.scala
@@ -16,6 +16,37 @@ class CamelCardsTest extends AnyFlatSpec {
     assert(Hand("23456").handType() === HighCard)
   }
 
+  "Hand comparison by hand type" should "be correct" in {
+    assert(Hand("AAAAA").compareTo(Hand("AAAAA")) === 0)
+    assert(Hand("AA8AA").compareTo(Hand("AA8AA")) === 0)
+    assert(Hand("23332").compareTo(Hand("23332")) === 0)
+    assert(Hand("TTT98").compareTo(Hand("TTT98")) === 0)
+    assert(Hand("23432").compareTo(Hand("23432")) === 0)
+    assert(Hand("A23A4").compareTo(Hand("A23A4")) === 0)
+    assert(Hand("23456").compareTo(Hand("23456")) === 0)
+
+    assert(Hand("AAAAA").compareTo(Hand("AA8AA")) > 0)
+    assert(Hand("AA8AA").compareTo(Hand("23332")) > 0)
+    assert(Hand("23332").compareTo(Hand("TTT98")) > 0)
+    assert(Hand("TTT98").compareTo(Hand("23432")) > 0)
+    assert(Hand("23432").compareTo(Hand("A23A4")) > 0)
+    assert(Hand("A23A4").compareTo(Hand("23456")) > 0)
+
+    assert(Hand("AA8AA").compareTo(Hand("AAAAA")) < 0)
+    assert(Hand("23332").compareTo(Hand("AA8AA")) < 0)
+    assert(Hand("TTT98").compareTo(Hand("23332")) < 0)
+    assert(Hand("23432").compareTo(Hand("TTT98")) < 0)
+    assert(Hand("A23A4").compareTo(Hand("23432")) < 0)
+    assert(Hand("23456").compareTo(Hand("A23A4")) < 0)
+  }
+
+  "Hand comparison by card strength" should "be correct" in {
+    assert(Hand("AAAAK").compareTo(Hand("AAAAQ")) > 0)
+    assert(Hand("AAAKA").compareTo(Hand("AAAQA")) > 0)
+    assert(Hand("AAKAA").compareTo(Hand("AAQAA")) > 0)
+    assert(Hand("AKAAA").compareTo(Hand("AQAAA")) > 0)
+  }
+
   "Total winnings for test data" should "be correct" in {
     assert(totalWinnings("Day07CamelCardsTestData.txt") === 6440)
   }
