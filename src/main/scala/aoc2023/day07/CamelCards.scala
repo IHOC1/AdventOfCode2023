c485e0902c6cf5a35e8dda37eab2f4c9f647c98a
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-09 10:00:22 +0000

    Correctly calculates the Part 2 total winnings for the test data

--- src/main/scala/aoc2023/day07/CamelCards.scala
+++ src/main/scala/aoc2023/day07/CamelCards.scala
@@ -23,6 +23,21 @@ object CamelCards {
       sum
   }
 
+  def totalWinningsWithJackWild(fileName: String): Int = {
+    import scala.io.Source
+
+    val source = Source.fromFile("src/test/resources/" + fileName)
+    val handsToBids = source.getLines().map(line => parseLine(line)).toMap
+    source.close()
+
+    val hands: Iterable[Hand] = handsToBids.keys.toList.sorted((x: Hand, y: Hand) => x.compareToJackWildAndWeakest(y))
+    val ranks = 1 until handsToBids.size + 1
+
+    hands.zip(ranks).toList.
+      map(p => handsToBids(p._1) * p._2).
+      sum
+  }
+
   def parseLine(line: String): (Hand, Int) = {
     val parts = line.split(" ")
 
@@ -42,15 +57,39 @@ case class Hand(cards: String) extends Comparable[Hand] {
 
   def handType() = {
     val cardGroups = cards.groupBy(c => c).values
-    val cardGroupSizes = cardGroups.map(_.length).toList.sorted
+    val sortedCardGroupSizes = cardGroups.map(_.length).toList.sorted
 
-         if (cardGroupSizes == List(5)         ) FiveOfAKind
-    else if (cardGroupSizes == List(1, 4)      ) FourOfAKind
-    else if (cardGroupSizes == List(2, 3)      ) FullHouse
-    else if (cardGroupSizes == List(1, 1, 3)   ) ThreeOfAKind
-    else if (cardGroupSizes == List(1, 2, 2)   ) TwoPair
-    else if (cardGroupSizes == List(1, 1, 1, 2)) OnePair
-    else                                         HighCard
+         if (sortedCardGroupSizes == List(5)         ) FiveOfAKind
+    else if (sortedCardGroupSizes == List(1, 4)      ) FourOfAKind
+    else if (sortedCardGroupSizes == List(2, 3)      ) FullHouse
+    else if (sortedCardGroupSizes == List(1, 1, 3)   ) ThreeOfAKind
+    else if (sortedCardGroupSizes == List(1, 2, 2)   ) TwoPair
+    else if (sortedCardGroupSizes == List(1, 1, 1, 2)) OnePair
+    else                                               HighCard
+  }
+
+  def handTypeWithWildJack() = {
+    val cardGroups = cards.groupBy(c => c).values
+    val sortedCardGroupSizes = cardGroups.map(_.length).toList.sorted
+
+    val handWithoutJacks = cards.filter(_ != 'J')
+    val numJacks = cards.count(_ == 'J')
+    val handWithoutJacksGroups = handWithoutJacks.groupBy(c => c).values
+    val handWithoutJacksSortedCardGroupSizes: Seq[Int] = handWithoutJacksGroups.map(_.length).toList.sorted
+
+    val deWildedSortedCardGroupSizes = if (numJacks == 5) {
+        sortedCardGroupSizes
+    } else {
+      handWithoutJacksSortedCardGroupSizes.reverse.tail.reverse :+ (handWithoutJacksSortedCardGroupSizes.reverse.head + numJacks)
+    }
+
+         if (deWildedSortedCardGroupSizes == List(5)         ) FiveOfAKind
+    else if (deWildedSortedCardGroupSizes == List(1, 4)      ) FourOfAKind
+    else if (deWildedSortedCardGroupSizes == List(2, 3)      ) FullHouse
+    else if (deWildedSortedCardGroupSizes == List(1, 1, 3)   ) ThreeOfAKind
+    else if (deWildedSortedCardGroupSizes == List(1, 2, 2)   ) TwoPair
+    else if (deWildedSortedCardGroupSizes == List(1, 1, 1, 2)) OnePair
+    else                                                       HighCard
   }
 
   val cardToStrength = Map('A' -> 13,
@@ -67,6 +106,20 @@ case class Hand(cards: String) extends Comparable[Hand] {
                            '3' -> 2,
                            '2' -> 1)
 
+  val cardToStrengthJackWeakest = Map('A' -> 13,
+                           'K' -> 12,
+                           'Q' -> 11,
+                           'T' -> 10,
+                           '9' -> 9,
+                           '8' -> 8,
+                           '7' -> 7,
+                           '6' -> 6,
+                           '5' -> 5,
+                           '4' -> 4,
+                           '3' -> 3,
+                           '2' -> 2,
+                           'J' -> 1)
+
   override def compareTo(other: Hand): Int = {
     -handType().compare(other.handType()) match {
       case       0 => compareHandsOnCardStrength(other)
@@ -82,6 +135,25 @@ case class Hand(cards: String) extends Comparable[Hand] {
 
   def compareCards(cards: (Char, Char)): Int = {
     cardToStrength(cards._1).compare(cardToStrength(cards._2))
+  }
+
+  // ----
+
+  def compareToJackWildAndWeakest(other: Hand): Int = {
+    -handTypeWithWildJack().compare(other.handTypeWithWildJack()) match {
+      case 0 => compareHandsOnCardStrengthJackWeakest(other)
+      case unequal => unequal
+    }
+  }
+
+  private def compareHandsOnCardStrengthJackWeakest(o: Hand) = {
+    cards.zip(o.cards).
+      map(pair => compareCardsJackWeakest(pair)).
+      find(comparison => comparison != 0).getOrElse(0)
+  }
+
+  def compareCardsJackWeakest(cards: (Char, Char)): Int = {
+    cardToStrength(cards._1).compare(cardToStrengthJackWeakest(cards._2))
   }
 
 }
--- src/test/scala/aoc2023/day07/CamelCardsTest.scala
+++ src/test/scala/aoc2023/day07/CamelCardsTest.scala
@@ -1,6 +1,6 @@
 package aoc2023.day07
 
-import aoc2023.day07.CamelCards.totalWinnings
+import aoc2023.day07.CamelCards.{totalWinnings, totalWinningsWithJackWild}
 import aoc2023.day07.HandType._
 import org.scalatest.flatspec.AnyFlatSpec
 
@@ -53,6 +53,40 @@ class CamelCardsTest extends AnyFlatSpec {
 
   "Total winnings for data" should "be correct" in {
     assert(totalWinnings("Day07CamelCards.txt") === 252295678)
+  }
+
+  // Part 2
+
+  "A Hand with jack as joker" should "have a type" in {
+    assert(Hand("AAAAA").handTypeWithWildJack() === FiveOfAKind)
+    assert(Hand("AA8AA").handTypeWithWildJack() === FourOfAKind)
+    assert(Hand("23332").handTypeWithWildJack() === FullHouse)
+    assert(Hand("TTT98").handTypeWithWildJack() === ThreeOfAKind)
+    assert(Hand("23432").handTypeWithWildJack() === TwoPair)
+    assert(Hand("A23A4").handTypeWithWildJack() === OnePair)
+    assert(Hand("23456").handTypeWithWildJack() === HighCard)
+
+    assert(Hand("AAAAJ").handTypeWithWildJack() === FiveOfAKind)
+    assert(Hand("AAAJA").handTypeWithWildJack() === FiveOfAKind)
+    assert(Hand("AAJAA").handTypeWithWildJack() === FiveOfAKind)
+    assert(Hand("AJAAA").handTypeWithWildJack() === FiveOfAKind)
+    assert(Hand("JAAAA").handTypeWithWildJack() === FiveOfAKind)
+
+    assert(Hand("JAAJJ").handTypeWithWildJack() === FiveOfAKind)
+    assert(Hand("JJJJJ").handTypeWithWildJack() === FiveOfAKind)
+
+    assert(Hand("AA8AA").handTypeWithWildJack() === FourOfAKind)
+    assert(Hand("AA8AA").handTypeWithWildJack() === FourOfAKind)
+
+    assert(Hand("32T3K").handTypeWithWildJack() === OnePair)
+    assert(Hand("T55J5").handTypeWithWildJack() === FourOfAKind)
+    assert(Hand("KK677").handTypeWithWildJack() === TwoPair)
+    assert(Hand("KTJJT").handTypeWithWildJack() === FourOfAKind)
+    assert(Hand("QQQJA").handTypeWithWildJack() === FourOfAKind)
+  }
+
+  "Part 2 total winnings for test data" should "be correct" in {
+    assert(totalWinningsWithJackWild("Day07CamelCardsTestData.txt") === 5905)
   }
 
 }
