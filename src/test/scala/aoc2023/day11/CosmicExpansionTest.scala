d8ee3af524a09b07149d385ff3b46d50d2c149d8
ian.haworth <137287384+ian-hmrc@users.noreply.github.com>
2023-12-19 16:45:41 +0000

    Passes test for the sum of shortest distances between galaxies for the test image.

---
+++ src/test/scala/aoc2023/day11/CosmicExpansionTest.scala
@@ -0,0 +1,11 @@
+package aoc2023.day11
+
+import aoc2023.day11.CosmicExpansion.parseImage
+import org.scalatest.flatspec.AnyFlatSpec
+
+class CosmicExpansionTest extends AnyFlatSpec {
+
+  "Test image" should "have sum of shortest distances between galaxies is 374" in {
+    assert(parseImage("Day11CosmicExpansionTest1.txt").sumOfShortestDistancesBetweenGalaxies() == 374L)
+  }
+}
